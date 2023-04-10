06d267d8add385afb95069fd86199bcf23e947b2
figsoda <figsoda@pm.me>
2023-04-10 17:04:16 -0400

    minor fixups

--- src/transformers/hoistLists.nix
+++ src/transformers/hoistLists.nix
@@ -1,41 +1,36 @@
-{ lib, super }: from: to:
+{ lib, super }:
+
+from: to:
 
 # Example from / to
 # - Lifting `imports` from: _imports, to: imports
 #
-# Note: underscore used as mere convention to
-#       signalling to the user the  "private"
-#       nature, they won't be part of the final
-#       view presented to the user
+# Note:
+#   underscore used as mere convention to signalling to the user the "private"
+#   nature, they won't be part of the final view presented to the user
 
 let
   inherit (builtins)
     removeAttrs
     ;
-
   inherit (lib)
     catAttrs
     concatLists
     ;
-
   inherit (super.utils)
     concatMapAttrsWith
     ;
 
-  /*mergeAttrsButConcatOn: merge attributes shallowly, but
-    concat values of a specific key into a list in that key
-
-    Type:
-      (key :: String -> AttrSet -> AttrSet) -> { ${key} :: List; ... }
-
-    */
+  # merge attributes shallowly, but concat values of a specific key into a list in that key
+  # Type: ((key : String) -> { ... } -> { ... }) -> { ${key} : [ a ], ... }
   mergeAttrsButConcatOn = key: x: y:
     x // y // {
       ${key} = concatLists (catAttrs key [ x y ]);
     };
-
 in
+
 cursor:
+
 if cursor == [ ] # toplevel
 then
   concatMapAttrsWith (mergeAttrsButConcatOn to)
