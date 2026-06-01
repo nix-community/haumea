2157348b0c5d12278fc07b5e14d259530b26855a
removed-user <caseyriley94@gmail.com>
2026-06-01 10:11:13 -0400

    Update flake.nix to use nixpkgs-lib

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -2,23 +2,24 @@
   inputs = {
     haumea = {
       url = "github:nix-community/haumea/v0.2.2";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
     };
-    nixpkgs.url = "github:nix-community/nixpkgs.lib";
+    nixpkgs-lib.url = "github:nix-community/nixpkgs.url";
+nixpkgs.url = "github:NixOS/nixpkgs-unstable";
   };
 
   outputs = { self, haumea, nixpkgs }: {
     checks = haumea.lib.loadEvalTests {
       src = ./tests;
       inputs = {
-        inherit (nixpkgs) lib;
+        inherit (nixpkgs-lib) lib;
         foo = self.lib;
       };
     };
     lib = haumea.lib.load {
       src = ./src;
       inputs = {
-        inherit (nixpkgs) lib;
+        inherit (nixpkgs-lib) lib;
       };
     };
   };
